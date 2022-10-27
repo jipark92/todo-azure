787dd8eb96dddb42cf752a828752c82b54559cdc
Ji Park <jiyongprk@gmail.com>
2022-10-27 15:50:25 -0400

    first commit

---
+++ src/redux/todoSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+
+export const todoSlice = createSlice({
+    name: 'todo',
+
+},
+})
+
+//actions
+export const { } = todoSlice.actions
+
+export default todoSlice.reducer
