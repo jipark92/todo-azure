4903c87134bbea88d24bf93518171202798e627e
Ji Park <jiyongprk@gmail.com>
2022-10-30 19:59:34 -0400

    render redux data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchTodos } from './redux/todoSlice'
 
 function App() {
+  const dispatch = useDispatch()
+  const todo = useSelector((state) => state.todo)
+
+  useEffect(() => {
+    dispatch(fetchTodos())
+  }, [])
+
+  console.log(todo.todos)
   return (
     <div className="App">
       hello world
+
+      {todo.todos.map((to) => {
+        return (
+          <li>{to.id}</li>
+        )
+      })}
+
     </div>
   );
 }
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,14 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
 
+// https://jsonplaceholder.typicode.com/todos
+const initialState = {
+    todos: [],
+}
+
+export const fetchTodos = createAsyncThunk('todo/fetchTodos', () => {
+    return axios
+        .get('https://jsonplaceholder.typicode.com/todos')
+        .then((res) => res.data)
+})
 
 export const todoSlice = createSlice({
     name: 'todo',
-
-},
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(fetchTodos.fulfilled, (state, action) => {
+            state.todos = action.payload
+        })
+        // buider
+    }
 })
-
-//actions
-export const { } = todoSlice.actions
 
 export default todoSlice.reducer
